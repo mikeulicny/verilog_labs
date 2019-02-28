a861eea5d276550c001d92dc36bd24b40c4a2a7c
mikeulicny <43107737+mikeulicny@users.noreply.github.com>
2019-02-27 20:55:55 -0500

    re-formatted d_flip_flop.v

--- sequential_circuits/D_flip_flop/D_flip_flop_tb.v
+++ sequential_circuits/D_flip_flop/D_flip_flop_tb.v
@@ -7,13 +7,13 @@ clk = 1'b0;
 always
 #10 clk = ~clk;
 initial
-begin
-	din = 1'b0; n_rst = 1'b1; #20
-				n_rst = 1'b0; #10
-	din = 1'b1;				  #25
-				n_rst = 1'b1; #10
-	din = 1'b0;				  #15
-	din = 1'b1;				  #20
-	din = 1'b0;				  #25
-end
+	begin
+		din = 1'b0; n_rst = 1'b1; 		  #20;
+					n_rst = 1'b0; 	  #10;
+		din = 1'b1;				  #25;
+					n_rst = 1'b1; 	  #10;
+		din = 1'b0;				  #15;
+		din = 1'b1;				  #20;
+		din = 1'b0;				  #25;
+	end
 endmodule
