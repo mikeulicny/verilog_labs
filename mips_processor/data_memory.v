5f26f4d0c4fc18a036cc7524852293d6b1fbbce2
mikeulicny <43107737+mikeulicny@users.noreply.github.com>
2020-07-29 20:05:42 -0400

    Formatting and documentation updates
    Formatting was changed to provide a more readable user code. Documentation was added to inputs and logic for readability

--- mips_processor/data_memory.v
+++ mips_processor/data_memory.v
@@ -2,25 +2,32 @@
 
 module data_memory (input clk,
  	// address input, shared by read and write port  
-	input [15:0] mem_access_addr,  
+	input [15:0] mem_access_addr,
+
 	// write port  
 	input [15:0] mem_write_data,  
+
+	// memory read/write flags
 	input mem_write_en,  
 	input mem_read,  
+
 	// read port  
-	output [15:0] mem_read_data);  
+	output [15:0] mem_read_data
+	);  
 	
 	integer i;  
 	reg [15:0] ram [255:0];  
 	wire [7 : 0] ram_addr = mem_access_addr[8 : 1];  
 	initial
-		begin  
-			for(i=0;i<256;i=i+1) ram[i] <= 16'd0;  
+		begin   // initialize all 256 bytes of RAM to 0000 0000 0000 0000
+			for(i=0; i<256; i=i+1) ram[i] <= 16'd0;  
 		end  
 		always @(posedge clk)
 		begin
+			// write data to RAM
 			if (mem_write_en)  
 				ram[ram_addr] <= mem_write_data;  
-		end  
+		end
+		// if memory read flag (mem_read) is true, read data from RAM
 		assign mem_read_data = (mem_read==1'b1) ? ram[ram_addr]: 16'd0;   
 endmodule  
