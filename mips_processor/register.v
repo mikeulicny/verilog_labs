0fd7c2a38bf2924049a6bacefa8bb399f0f1b3f8
mikeulicny <43107737+mikeulicny@users.noreply.github.com>
2020-07-29 20:20:15 -0400

    Improved formatting for readability

--- mips_processor/register.v
+++ mips_processor/register.v
@@ -1,17 +1,21 @@
 // register for mips processor
 
 module register_file (input clk,
-				input rst,
-				// write port
-				input reg_write_en,
-				input [2:0] reg_write_dest,
-				input [15:0] reg_write_data,  
- 				//read port 1
-				input [2:0] reg_read_addr_1,  
-				output [15:0] reg_read_data_1,  
- 				//read port 2  
- 				input [2:0] reg_read_addr_2,  
-				output [15:0] reg_read_data_2);
+		      input rst,
+		
+		      // write port
+		      reg_write_en,
+		      input [2:0] reg_write_dest,
+		      input [15:0] reg_write_data,  
+		      
+		      //read port 1
+		      input [2:0] reg_read_addr_1,  
+		      output [15:0] reg_read_data_1,  
+		      
+		      //read port 2  
+		      input [2:0] reg_read_addr_2,  
+		      output [15:0] reg_read_data_2
+		     );
 
  	reg [15:0] reg_array [7:0];  
  	// write port  
@@ -29,12 +33,11 @@ module register_file (input clk,
 				reg_array[7] <= 16'b0;       
 				end  
 			else begin  
-				if(reg_write_en)
-					begin  
- 						reg_array[reg_write_dest] <= reg_write_data;  
-					end  
- 				end  
- 		end  
+				if(reg_write_en) begin  
+ 					reg_array[reg_write_dest] <= reg_write_data;  
+				end  
+ 			end  
+ 	end  
 	assign reg_read_data_1 = ( reg_read_addr_1 == 0)? 16'b0 : reg_array[reg_read_addr_1];  
 	assign reg_read_data_2 = ( reg_read_addr_2 == 0)? 16'b0 : reg_array[reg_read_addr_2];  
 endmodule
